1d90af6a145fbf5edd630dc8932b376d2f524ce4
shyamharshit <shyamfreefire499@gmail.com>
2025-04-08 19:12:24 +0530

    ddictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+dictionary={1:"a" ,2:"b" ,3:"c"}
+print(dictionary)
+
+dictionary.update({2:"blue"})
+print(dictionary)
+
+dictionary.pop(3)
+print(dictionary)
+
+del dictionary[2]
+print(dictionary)
